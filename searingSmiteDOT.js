2740ba55b1afe10bbc782b7d5fda96400b3a30cf
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-02-22 12:03:05 -0500

    searing smite

---
+++ searingSmiteDOT.js
@@ -0,0 +1,6 @@
+﻿(async () => {
+    let target = canvas.tokens.objects.children.find(i => i.data._id == args[0]);
+    let numDice = 1;
+    let damageRoll = new Roll(`${numDice}d6[fire]`).roll();
+    new MidiQOL.DamageOnlyWorkflow(actor, token, damageRoll.total, "fire", [target], damageRoll, { flavor: "Searing Smite - Damage (fire)" });
+})();
