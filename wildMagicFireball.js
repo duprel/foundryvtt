1bbef907634931c2b972adf30b4257dfb86b43c2
Lance duPre <ldupre237@gmail.com>
2021-03-01 15:44:37 -0500

    wildMagicFrieball script

---
+++ wildMagicFireball.js
@@ -0,0 +1,8 @@
+﻿(async () => {
+    if (canvas.tokens.controlled.length != 1) {
+        ui.notifications.error("Please select a single token.");
+        return;
+    }
+    let dc = canvas.tokens.controlled[0].actor.data.data.abilities.cha.dc;
+    let surge = new Roll(`8d6[fire]`).roll().toMessage({ flavor: "Wild Magic Surge! 3rd level fireball centered on the caster! DEX save DC: " + dc });
+})();
