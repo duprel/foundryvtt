6c137d9828fdd1cecf847e00aecaa52acde84261
Lance duPre <ldupre237@gmail.com>
2021-03-01 16:09:50 -0500

    wildMagicGrowShrink script

---
+++ wildMagicGrowShrink.js
@@ -0,0 +1,11 @@
+﻿(async () => {
+    let surge = new Roll(`d10`).roll();
+    if (surge._total % 2 == 0) {
+        var messageContent = "You grow " + surge._total + " inch(es)"
+    } else {
+        var messageContent = "You shrink " + surge._total + " inch(es)"
+    };
+    surge.toMessage({});
+    var chatData = { content: messageContent };
+    ChatMessage.create(chatData, {});
+})();
