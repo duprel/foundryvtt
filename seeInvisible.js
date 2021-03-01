b2f9c13edb8ccf510c7933ed7f8ddcb7764bc9fe
Lance duPre <ldupre237@gmail.com>
2021-03-01 15:27:38 -0500

    seeInvisibility script

---
+++ seeInvisible.js
@@ -0,0 +1,8 @@
+﻿(async () => {
+    if (canvas.tokens.controlled.length != 1) {
+        ui.notifications.error("Please select a single token.");
+        return;
+    }
+    let contrChar = canvas.tokens.controlled[0];
+    contrChar.update({ "flags.conditional-visibility.seeinvisible": true });
+})();
