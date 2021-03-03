d0ddd61659815a811cd685b335f18d28b5d55e9e
Lance duPre <ldupre237@gmail.com>
2021-03-03 11:10:40 -0500

    Wild magic aging

---
+++ wildMagicAge.js
@@ -0,0 +1,11 @@
+﻿(async () => {
+    let surge = new Roll(`d10`).roll();
+    if (surge._total % 2 == 0) {
+        var messageContent = "You grow " + surge._total + " year(s) older!"
+    } else {
+        var messageContent = "You grow " + surge._total + " year(s) younger!"
+    };
+    surge.toMessage({});
+    var chatData = { content: messageContent };
+    ChatMessage.create(chatData, {});
+})();
