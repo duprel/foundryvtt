55bbf1d91a66e0659ba4cdb3672f513d9c42f512
Lance duPre <ldupre237@gmail.com>
2021-03-04 08:05:52 -0500

    Wild magic

---
+++ wildMagicHeal.js
@@ -0,0 +1,8 @@
+﻿(async () => {
+    let numDice =2;
+    let target = canvas.tokens.controlled[0];
+    let healingRoll = new Roll(`${numDice}d10[healing]`).roll();
+    let healingTotal = healingRoll.total;
+    console.log(healingTotal);
+    new MidiQOL.DamageOnlyWorkflow(actor, token, healingRoll.total, "healing", [target], healingRoll, { flavor: "You heal " + healingTotal + " HP!" })
+})();
---
+++ wildMagicLightning.js
@@ -0,0 +1,15 @@
+﻿(async () => {
+    if (game.user.targets.size > 3 || game.user.targets.size < 1) {
+        ui.notifications.error("Please target 1 to 3 tokens.");
+        return;
+    };
+    var targets = Array.from(game.user.targets);
+    var numDice = 4;
+    //let lightningRoll = new Roll(`${numDice}d10[lightning]`).roll();
+    for (let i = 0; i < targets.length; i++) {
+        let target = targets[i];
+        let lightningRoll = new Roll(`${numDice}d10[lightning]`).roll();
+        new MidiQOL.DamageOnlyWorkflow(actor, token, lightningRoll.total, "lightning", [target], lightningRoll, {})
+    };
+})();
+
