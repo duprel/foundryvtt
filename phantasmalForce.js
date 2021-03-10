0ab97e607ee64091769755b29c6ad0801e0593a1
Lance duPre <ldupre237@gmail.com>
2021-03-10 16:39:18 -0500

    phantasmal force

---
+++ phantasmalForce.js
@@ -0,0 +1,12 @@
+﻿//args[1] = @token
+(async () => {
+    if (args[0] === "on") {
+        const item = game.items.getName("Phantasmal Force Damage");
+        let target = canvas.tokens.get(args[1]);
+        target.actor.createOwnedItem(item.data);
+    } else {
+        let token = canvas.tokens.get(args[1]);
+        const itemId = token.actor.items.getName("Phantasmal Force Damage");
+        token.actor.deleteOwnedItem(itemId.data._id);
+    };
+})();
---
+++ wildMagicNecrotic.js
@@ -0,0 +1,14 @@
+﻿(async () => {
+    if (game.user.targets.size < 1) {
+        ui.notifications.error("Please target at least 1 token.");
+        return;
+    };
+    var targets = Array.from(game.user.targets);
+    var numDice = 1;
+    for (let i = 0; i < targets.length; i++) {
+        let target = targets[i];
+        let necroticRoll = new Roll(`${numDice}d10[necrotic]`).roll();
+        new MidiQOL.DamageOnlyWorkflow(actor, token, necroticRoll.total, "necrotic", [target], necroticRoll, {})
+    };
+})();
+
