2abe3c6458d7fd11fd737cd1ae779d5093b71c0f
Lance duPre <ldupre237@gmail.com>
2021-03-13 11:07:15 -0500

    goodberry

--- goodberry.js
+++ goodberry.js
@@ -1,12 +1,67 @@
-﻿(async () => {
-    if (args[0] === "on") {
-        const item = game.items.getName("Goodberries");
-        let target = canvas.tokens.get(args[1]);
-        target.actor.createOwnedItem(item.data);
-    } else {
-        var tokenId = args[2].tokenId;
-        let token = canvas.tokens.get(tokenId);
-        const itemId = token.actor.items.getName("Goodberries");
-        token.actor.deleteOwnedItem(itemId.data._id);
-    }
-})();
+﻿//The spell has to expire for the goodberries to be deleted. Otherwise the effect must be deleted on the character sheet (not toggled)
+if (game.user.targets.size != 1) {
+    ui.notifications.error("Please target a single token.");
+    return;
+};
+var t = Array.from(game.user.targets)[0];
+if (args[0] === "on") {
+    let data = {};
+    await t.actor.createOwnedItem(
+        {
+            "name": "Goodberries",
+            "type": "consumable",
+            "data": {
+                "quantity": 10,
+                "activation": {
+                    "type": "action",
+                    "cost": 1,
+                    "condition": ""
+                },
+                "duration": {
+                    "value": 24,
+                    "units": "hour"
+                },
+                "target": {
+                    "value": 1,
+                    "width": null,
+                    "units": "",
+                    "type": "creature"
+                },
+                "range": {
+                    "value": null,
+                    "long": null,
+                    "units": "creature"
+                },
+                "uses": {
+                    "autodestroy": true,
+                    "autouse": true,
+                    "value": 10,
+                    "max": 10,
+                    "per": "charges"
+                },
+                "consume": {
+                    "type": "",
+                    "target": "",
+                    "amount": null
+                },
+                "ability": "",
+                "actionType": "heal",
+                "attackBonus": "0",
+                "chatFlavor": "",
+                "critical": null,
+                "damage": {
+                    "parts": [
+                        [
+                            "1",
+                            "healing"
+                        ]
+                    ],
+                },
+            },
+            "img": "media/images/WoW/Miscellaneous/Organ_06.webp",
+        }
+    );
+} else {
+    let item = t.actor.data.items.find(i => i.name === "Goodberries" && i.type === "consumable")
+    t.actor.deleteOwnedItem(item._id)
+};
