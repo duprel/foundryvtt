3344290bad7ba3cc5dcfa44b0e59c9cf798b720b
Lance duPre <ldupre237@gmail.com>
2021-03-14 11:35:13 -0400

    Magic Stone Incomplete

---
+++ magicStone.js
@@ -0,0 +1,70 @@
+﻿//The spell has to expire for the Magic Stones to be deleted. Otherwise the effect must be deleted on the character sheet (not toggled)
+if (game.user.targets.size != 1) {
+    ui.notifications.error("Please target a single token.");
+    return;
+};
+var t = Array.from(game.user.targets)[0];
+if (args[0] === "on") {
+    let data = {};
+    await t.actor.createOwnedItem(
+        {
+            "name": "Magic Stones",
+            "type": "weapon",
+            "data": {
+                "quantity": 1,
+                "activation": {
+                    "type": "action",
+                    "cost": 1,
+                    "condition": ""
+                },
+                "duration": {
+                    "value": null,
+                    "units": ""
+                },
+                "target": {
+                    "value": 1,
+                    "width": null,
+                    "units": "",
+                    "type": "creature"
+                },
+                "range": {
+                    "value": null,
+                    "long": 60,
+                    "units": "feet"
+                },
+                "uses": {
+                    "autodestroy": true,
+                    "autouse": true,
+                    "value": 3,
+                    "max": 3,
+                    "per": "charges"
+                },
+                "consume": {
+                    "type": "",
+                    "target": "",
+                    "amount": null
+                },
+                "ability": "",
+                "actionType": "rsak",
+                "attackBonus": "0",
+                "chatFlavor": "",
+                "critical": null,
+                "damage": {
+                    "parts": [
+                        [
+                            "1d6 + @abilities.wis.mod",
+                            "bludgeoning"
+                        ]
+                    ],
+                },
+                "weaponType": "simpleR",
+                "proficient": true,
+                "equipped": true,
+            },
+            "img": "modules/plutonium/media/icon/spell/xge-magic-stone.jpg",
+        }
+    );
+} else {
+    let item = t.actor.data.items.find(i => i.name === "Magic Stones" && i.type === "weapon")
+    t.actor.deleteOwnedItem(item._id)
+};
