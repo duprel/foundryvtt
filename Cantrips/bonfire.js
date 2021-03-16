d08196e60f901ea2f3436f0ca33adeae38949837
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-03-16 17:02:32 -0400

    Create Bonfire Incomplete

---
+++ Cantrips/bonfire.js
@@ -0,0 +1,15 @@
+﻿(async () => {
+    if (canvas.tokens.controlled.length != 1) {
+        ui.notifications.error("Please select a single token.");
+        return;
+    };
+    var t = canvas.tokens.controlled[0].actor;
+    if (args[0] === "on") {
+        let tName = t.data.name;
+        Summoner.placeAndSummon(
+            t,
+            "Bonfire",
+        );
+    } else {
+    };
+})();
