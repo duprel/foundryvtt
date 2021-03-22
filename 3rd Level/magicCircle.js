44ab9b19d89b5a9a224153fc6f1da51719fd37b1
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-03-22 17:29:38 -0400

    3rd Level

---
+++ 3rd Level/magicCircle.js
@@ -0,0 +1,12 @@
+﻿(async () => {
+    if (canvas.tokens.controlled.length != 1) {
+        ui.notifications.error("Please select a single token.");
+        return;
+    }
+    let a = canvas.tokens.controlled[0].actor;
+    Summoner.placeAndSummon(
+        a,
+        "Magic Circle",
+    );
+})();
+
