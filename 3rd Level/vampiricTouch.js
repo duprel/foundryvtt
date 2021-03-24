076a6693116d99e99c85de93287d2fdb27a2eaf0
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-03-24 17:19:52 -0400

    Inomplete

---
+++ 3rd Level/vampiricTouch.js
@@ -0,0 +1,59 @@
+﻿//args[1] = spell level
+(async () => {
+    if (canvas.tokens.controlled.length != 1) {
+        ui.notifications.error("Please select a single token.");
+        return;
+    }
+    var t = canvas.tokens.controlled[0];
+    if (args[0] === "on") {
+        let diceNum = args[1];
+        let data = {};
+        await t.actor.createOwnedItem(
+            {
+                "name": "Vampiric Touch Attack",
+                "type": "weapon",
+                "data": {
+                    "quantity": 1,
+                    "activation": {
+                        "type": "action",
+                        "cost": 1,
+                        "condition": ""
+                    },
+                    "duration": {
+                        "value": 1,
+                        "units": "minute"
+                    },
+                    "target": {
+                        "value": 1,
+                        "width": null,
+                        "units": "",
+                        "type": "creature"
+                    },
+                    "range": {
+                        "value": 5,
+                        "long": null,
+                        "units": "ft"
+                    },
+                    "ability": "",
+                    "actionType": "msak",
+                    "attackBonus": "0",
+                    "chatFlavor": "",
+                    "equipped": true,
+                    "critical": null,
+                    "damage": {
+                        "parts": [
+                            [
+                                `${diceNum}d8`,
+                                "necrotic"
+                            ]
+                        ],
+                    },
+                },
+                "img": "systems/dnd5e/icons/spells/rip-sky-2.jpg",
+            }
+        );
+    } else {
+        let item = t.actor.data.items.find(i => i.name === "Vampiric Touch Attack" && i.type === "weapon")
+        t.actor.deleteOwnedItem(item._id)
+    };
+})();
