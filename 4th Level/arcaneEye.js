097b4740525dd528ab195ee11a9c2c1b46f55a31
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-03-29 16:19:43 -0400

    Complete

---
+++ 4th Level/arcaneEye.js
@@ -0,0 +1,16 @@
+﻿(async () => {
+    if (canvas.tokens.controlled.length != 1) {
+        ui.notifications.error("Please select a single token.");
+        return;
+    }
+    var a = canvas.tokens.controlled[0].actor;
+    if (args[0] === "on") {
+        console.log("FART");
+        await Summoner.placeAndSummon(
+            a,
+            "Arcane Eye Summon",
+        ); 
+    } else {
+        await Summoner.dismiss("Arcane Eye Summon");
+    };
+})();
