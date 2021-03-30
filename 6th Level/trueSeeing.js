a5a3ae955531a7324c1b09fcd460838bc055f31c
Lance duPre <ldupre237@gmail.com>
2021-03-29 21:02:48 -0400

    complete

---
+++ 6th Level/trueSeeing.js
@@ -0,0 +1,9 @@
+﻿//The spell has to expire for truesight to be toggled off. Otherwise the effect must be deleted on the character sheet (not toggled) 
+(async () => {
+    let t = Array.from(game.user.targets)[0];
+    if (args[0] === "on") {
+        await t.update({ "flags.conditional-visibility.truesight": true });
+    } else {
+        await t.update({ "flags.conditional-visibility.truesight": false });
+    };
+})();
