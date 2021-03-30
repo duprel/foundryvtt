fedcd8ee688e12827b10ba5df598fcec2ea105fa
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-03-30 13:09:10 -0400

    Complete

---
+++ Items/elixirOfHealth.js
@@ -0,0 +1,16 @@
+﻿(async () => {
+    if (args[0] === "on") {
+        if (game.cub.hasCondition("Blinded")) {
+            game.cub.removeCondition("Blinded");
+        };
+        if (game.cub.hasCondition("Deafened")) {
+            game.cub.removeCondition("Deafened");
+        };
+        if (game.cub.hasCondition("Paralyzed")) {
+            game.cub.removeCondition("Paralyzed");
+        };
+        if (game.cub.hasCondition("Poisoned")) {
+            game.cub.removeCondition("Poisoned");
+        };
+    };
+})();
