d2c1318840c33427998eb8da2983542f25f25671
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-03-30 11:21:35 -0400

    Complete

---
+++ 7th Level/teleport.js
@@ -0,0 +1,56 @@
+﻿if (args[0] === "on") {
+    new Dialog({
+        title: "Choose location familiarity",
+        buttons: {
+            one: {
+                label: "Permanent Circle",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: Permanent Circle");
+                    table.draw();
+                }
+            },
+            two: {
+                label: "Associated Object",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: Associated Object");
+                    table.draw();
+                }
+            },
+            three: {
+                label: "Very Familiar",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: Very Familiar");
+                    table.draw();
+                }
+            },
+            four: {
+                label: "Seen Casually",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: Seen Casually");
+                    table.draw();
+                }
+            },
+            five: {
+                label: "Viewed Once",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: Viewed Once");
+                    table.draw();
+                }
+            },
+            six: {
+                label: "Description",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: Description");
+                    table.draw();
+                }
+            },
+            seven: {
+                label: "False Destination",
+                callback: () => {
+                    const table = game.tables.entities.find(t => t.name === "Teleport: False Destination");
+                    table.draw();
+                }
+            }
+        }
+    }).render(true);
+}
