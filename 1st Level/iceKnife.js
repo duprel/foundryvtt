987beeb5d66004b679a3dd88838f9787a7a083ef
Lance duPre <ldupre237@gmail.com>
2021-04-01 18:32:55 -0400

    incomplete

---
+++ 1st Level/iceKnife.js
@@ -0,0 +1,10 @@
+﻿(async () => {
+    if (args[0] === "on") {
+        let spellDC = canvas.tokens.controlled[0].actor.data.data.attributes.spelldc;
+        let numDice = args[1] + 1
+        let tactor = game.actors.entities.find(a => a.name === "RiderEffects");
+        let item = tactor.items.find(i => i.name === "Ice Knife Explosion");
+        console.log("###############:        ", numDice);
+    } else {
+    };
+})();
