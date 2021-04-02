edd840895bf9a44c584bb1d7f7ee6534d1af0a6a
Lance duPre <ldupre237@gmail.com>
2021-04-01 21:15:46 -0400

    complete

--- 1st Level/iceKnife.js
+++ 1st Level/iceKnife.js
@@ -1,10 +1,28 @@
 ﻿(async () => {
     if (args[0] === "on") {
         let spellDC = canvas.tokens.controlled[0].actor.data.data.attributes.spelldc;
-        let numDice = args[1] + 1
-        let tactor = game.actors.entities.find(a => a.name === "RiderEffects");
-        let item = tactor.items.find(i => i.name === "Ice Knife Explosion");
-        console.log("###############:        ", numDice);
+        let tactor = Array.from(game.user.targets)[0];
+        let numDice = (args[1] + 1)
+        let effectActor = game.actors.entities.find(a => a.name === "RiderEffects");
+        let effect = effectActor.items.find(i => i.name === "Ice Knife Explosion");
+        await effect.update(
+            {
+                "data": {
+                    "save": {
+                        "dc": spellDC
+                    },
+                    "damage": {
+                        "parts": [
+                            [
+                                `${numDice}d6`,
+                                "cold"
+                            ]
+                        ],
+                    },
+                },     
+          }
+        );
+        new MidiQOL.TrapWorkflow(tactor, effect, [tactor], tactor.center)
     } else {
     };
 })();
