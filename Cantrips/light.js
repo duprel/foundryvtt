992d5a81716f6ce3756208119c55f834e126f1e0
Lance duPre <ldupre237@gmail.com>
2021-04-11 10:12:10 -0400

    complete

---
+++ Cantrips/light.js
@@ -0,0 +1,11 @@
+﻿(async () => {
+    if (game.user.targets.size != 1) {
+        ui.notifications.error("Please target a single token.");
+        return;
+    };
+    let target = Array.from(game.user.targets)[0];
+    target.update({
+        dimLight: 40,
+        brightLight: 20
+    });
+})();
