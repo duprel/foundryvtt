8a6871ca5089489e59b771bc78203ac55bef8b7a
Lance duPre <ldupre237@gmail.com>
2021-04-11 18:41:55 -0400

    complete

---
+++ 3rd Level/daylight.js
@@ -0,0 +1,12 @@
+﻿//you have to delete the effect, not deactivate it.
+(async () => {
+    var a = canvas.tokens.controlled[0].actor;
+    if (args[0] === "on") {
+        await Summoner.placeAndSummon(
+            a,
+            "Daylight Spell",
+        );
+    } else {
+        await Summoner.dismiss("Daylight Spell");
+    };
+})();
