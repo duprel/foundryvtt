4004cfdc44e0ed256e4ce743f9e487384a8ecb0f
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-13 09:53:49 -0400

    complete

--- Items/potionOfFireBreath.js
+++ Items/potionOfFireBreath.js
@@ -1,65 +1,80 @@
 ﻿//DAE Macro Execute, Effect Value = "Macro Name"
 
 let t = canvas.tokens.controlled[0];
-let data = {}
+let data = {};
 /**
  * Create Fire Breath item in inventory
  */
-if (args[0] === "on") {
-    await t.actor.createOwnedItem(
-        {   
-            "data": {
-                "actionType": "save",
-                "damage": {
-                    "parts": [
-                        [
-                            "4d6",
-                            "fire"
-                        ]
-                    ],
-                },
-                "equipped": true,
-                "quantity": 1,
-                
-                "activation": {
-                    "type": "action",
-                    "cost": 1,
-                    "condition": ""
-                },
-                "proficient": true,
-                "properties": {
-                    "halfdam": true,
-                },
-                "range": {
-                    "value": 30,
-                    "long": null,
-                    "units": "feet"
-                },
-                "save": {
-                    "ability": "dex",
-                    "dc": 13,
-                    "scaling": "flat"
-                },
-                "target": {
-                    "value": 1,
-                    "type": "creature"
-                },
-                "uses": {
-                    "max": 3,
-                    "per": "charges",
-                    "value": 0 
-                },
-                "weaponType": "simpleM",
+await t.actor.createOwnedItem(
+    {
+        "data": {
+            "actionType": "save",
+            "damage": {
+                "parts": [
+                    [
+                        "4d6",
+                        "fire"
+                    ]
+                ],
             },
-            "img": "modules/plutonium/media/icon/spell/xge-dragons-breath.jpg",
-            "name": "Fire Breath",
-            "type": "weapon",
-        }
-    );
-}
+            "equipped": true,
+            "quantity": 1,
+            "activation": {
+                "type": "action",
+                "cost": 1,
+                "condition": ""
+            },
+            "proficient": true,
+            "properties": {
+                "halfdam": true,
+            },
+            "range": {
+                "value": 30,
+                "long": null,
+                "units": "feet"
+            },
+            "save": {
+                "ability": "dex",
+                "dc": 13,
+                "scaling": "flat"
+            },
+            "target": {
+                "value": 1,
+                "type": "creature"
+            },
+            "uses": {
+                "autoDestroy": true,
+                "autoUse": true,
+                "max": 3,
+                "per": "charges",
+                "value": 3,
+            },
+            "weaponType": "simpleM",
+        },
+        "flags": {
+            "autoanimations": {
+                "animName": "Fire Bolt",
+                "animTint": "#ffffff",
+                "animType": "t6",
+                "auraOpacity": 0.75,
+                "color": "orange",
+                "ctaOption": false,
+                "dtvar": "dt1",
+                "explodeColor": "ec1",
+                "explodeLoop": "1",
+                "explodeRadius": "0",
+                "explodeVariant": "ev1",
+                "explosion": false,
+                "hmAnim": "a1",
+                "killAnim": false,
+                "override": true,
+                "selfRadius": "5",
+            },
+        },
+        "img": "modules/plutonium/media/icon/spell/xge-dragons-breath.jpg",
+        "name": "Fire Breath",
+        "type": "weapon",
+    }
+);
 
-// Delete Flame Blade
-if (args[0] === "off") {
-    let item = target.actor.data.items.find(i => i.name === "Summoned Flame Blade" && i.type === "weapon");
-    target.actor.deleteOwnedItem(item._id)
-}
+
