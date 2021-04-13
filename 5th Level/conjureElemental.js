4ebf15c87d264141a6fc221e69d518a05312c799
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-13 17:46:30 -0400

    complete

---
+++ 5th Level/conjureElemental.js
@@ -0,0 +1,63 @@
+﻿(async () => {
+    var a = canvas.tokens.controlled[0].actor;
+    let changes = {
+        data: {
+            details: {
+                cr: args[0].spellLevel
+            },
+        }
+    };
+    new Dialog({
+        title: "Choose the Elemental Summoned.",
+        buttons: {
+            one: {
+                label: "Air",
+                callback: () => {
+                    Summoner.placeAndSummon(
+                        a,
+                        "Air Elemental",
+                        {
+                            actorData: changes,
+                        },
+                    );                       
+                }
+            },
+            two: {
+                label: "Earth",
+                callback: () => {
+                    Summoner.placeAndSummon(
+                        a,
+                        "Earth Elemental",
+                        {
+                            actorData: changes,
+                        },
+                    );
+                }
+            },
+            three: {
+                label: "Fire",
+                callback: () => {
+                    Summoner.placeAndSummon(
+                        a,
+                        "Fire Elemental",
+                        {
+                            actorData: changes,
+                        },
+                    );
+                }
+            },
+            four: {
+                label: "Water",
+                callback: () => {
+                    Summoner.placeAndSummon(
+                        a,
+                        "Water Elemental",
+                        {
+                            actorData: changes,
+                        },
+                    );
+                }
+            }
+        }
+    }).render(true);
+})();
---
+++ Macros/elementalGemEarth.js
@@ -0,0 +1,8 @@
+﻿//Macro args[1] = passed from Item Macro
+(async () => {
+    var a = canvas.tokens.controlled[0].actor;
+    await Summoner.placeAndSummon(
+        a,
+        "Earth Elemental",
+    );
+})();
