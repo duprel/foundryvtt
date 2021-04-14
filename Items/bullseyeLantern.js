05c5f842ee5d9c37a85c4d9d3cdc5d19e9feafb7
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-14 17:04:22 -0400

    complete

---
+++ Items/bullseyeLantern.js
@@ -0,0 +1,18 @@
+﻿(async () => {
+    let t = canvas.tokens.controlled[0];
+    if (args[0] === "on") {
+        t.update({
+            dimLight: 60,
+            brightLight: 30,
+            lightAnimation: { type: "torch", speed: 2, intensity: 2 },
+            lightAlpha: 0.15,
+            lightColor: "#f8c377"
+        });    
+    } else {
+        t.update({
+            dimLight: 0,
+            brightLight: 0,
+        });
+    };
+})();
+
