9262e00574a895755827dad5778045567c43b7c2
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-14 17:37:33 -0400

    incomplete

---
+++ 2nd Level/spiritualWeapon.js
@@ -0,0 +1,15 @@
+﻿//Macro args[1] = passed from Item Macro
+(async () => {
+    var a = canvas.tokens.controlled[0].actor;
+    if (args[0] === "on") {
+        await Summoner.placeAndSummon(
+            a,
+            "Summoned Spiritual Weapon",
+        );
+        var weapon = await canvas.tokens.objects.children.find(i => i.data.name == "Spiritual Weapon*");
+        console.log("#############:      ", weapon);
+    } else {
+        await Summoner.dismiss("Summoned Spiritual Weapon");
+    };
+    
+})();
