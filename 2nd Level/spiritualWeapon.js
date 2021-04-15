db4f7fc0fc0723cf2cb20795cded623bf5e1a64c
Lance duPre <ldupre237@gmail.com>
2021-04-15 11:55:45 -0400

    complete

--- 2nd Level/spiritualWeapon.js
+++ 2nd Level/spiritualWeapon.js
@@ -1,15 +1,65 @@
-﻿//Macro args[1] = passed from Item Macro
-(async () => {
-    var a = canvas.tokens.controlled[0].actor;
-    if (args[0] === "on") {
-        await Summoner.placeAndSummon(
-            a,
-            "Summoned Spiritual Weapon",
-        );
-        var weapon = await canvas.tokens.objects.children.find(i => i.data.name == "Spiritual Weapon*");
-        console.log("#############:      ", weapon);
-    } else {
-        await Summoner.dismiss("Summoned Spiritual Weapon");
-    };
-    
-})();
+﻿//DAE Macro Execute, Effect Value = "Macro Name" @target @item.level
+let target = canvas.tokens.get(args[1]);
+let data = {}
+const lastArg = args[args.length - 1];
+const item = lastArg.efData.flags.dae.itemData;
+if (args[0] === "on") {
+    let image = item.img;
+    let damage = Math.floor((parseInt(args[2]) / 2));
+    await target.actor.createOwnedItem(
+        {
+            "name": "Summoned Spiritual Weapon",
+            "type": "weapon",
+            "data": {
+                "equipped": true,
+                "identified": true,
+                "activation": {
+                    "type": "bonus",
+                },
+                "target": {
+                    "value": 1,
+                    "width": null,
+                    "type": "creature"
+                },
+                "range": {
+                    "value": 5,
+                    "units": "ft"
+                },
+                "ability": "",
+                "actionType": "msak",
+                "attackBonus": "0",
+                "chatFlavor": "",
+                "critical": null,
+                "damage": {
+                    "parts": [
+                        [
+                            `${damage}d8+@mod`,
+                            "force"
+                        ]
+                    ],
+                },
+                "weaponType": "simpleM",
+                "proficient": true
+            },
+            "flags": {
+                "DAESRD": {
+                    "SpiritualWeapon":
+                        target.actor.id
+                }
+            },
+            "img": `${image}`,
+        },
+    );
+    ui.notifications.notify("Weapon created in your inventory");
+    await Summoner.placeAndSummon(
+        target.actor,
+        "Summoned Spiritual Weapon",
+    );
+} else {
+    let item = target.actor.items.find(i => i.data.flags?.DAESRD?.SpiritualWeapon === target.actor.id)
+    let template = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.SpiritualWeapon?.ActorId === target.actor.id)
+    await target.actor.deleteOwnedItem(item.id);
+    await Summoner.dismiss("Summoned Spiritual Weapon");
+};
+
+
