e512de1c32c2c686aa1c5b63294aea90fc554c00
Lance duPre <ldupre237@gmail.com>
2021-04-17 10:11:30 -0400

    complete

---
+++ Items/featherTokenWhip.js
@@ -0,0 +1,65 @@
+﻿//DAE Macro Execute, Effect Value = "Macro Name" @target 
+let target = canvas.tokens.get(args[1]);
+let data = {}
+const lastArg = args[args.length - 1];
+const item = lastArg.efData.flags.dae.itemData;
+if (args[0] === "on") {
+    let image = item.img;
+    let damage = Math.floor((parseInt(args[2]) / 2));
+    await target.actor.createOwnedItem(
+        {
+            "name": "Quaal's Feather Token: Whip",
+            "type": "weapon",
+            "data": {
+                "equipped": true,
+                "identified": true,
+                "activation": {
+                    "type": "bonus",
+                },
+                "target": {
+                    "value": 1,
+                    "width": null,
+                    "type": "creature"
+                },
+                "range": {
+                    "value": 5,
+                    "units": "ft"
+                },
+                "ability": "",
+                "actionType": "msak",
+                "attackBonus": "9",
+                "chatFlavor": "",
+                "critical": null,
+                "damage": {
+                    "parts": [
+                        [
+                            "1d6+5",
+                            "force"
+                        ]
+                    ],
+                },
+                "weaponType": "simpleM",
+                "proficient": false
+            },
+            "flags": {
+                "DAESRD": {
+                    "FeatherToken":
+                        target.actor.id
+                }
+            },
+            "img": `${image}`,
+        },
+    );
+    ui.notifications.notify("Weapon created in your inventory");
+    await Summoner.placeAndSummon(
+        target.actor,
+        "Summoned Feather Token",
+    );
+} else {
+    let item = target.actor.items.find(i => i.data.flags?.DAESRD?.FeatherToken === target.actor.id)
+    let template = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.FeatherToken?.ActorId === target.actor.id)
+    await target.actor.deleteOwnedItem(item.id);
+    await Summoner.dismiss("Summoned Feather Token");
+};
+
+
---
+++ Macros/lifeDrain.js
@@ -0,0 +1,24 @@
+﻿//args[1] = @target, args[2] = @damage  
+(async () => {
+    let t = canvas.tokens.get(args[1]).actor;
+    if (args[0] === "on") {
+        let max = t.data.data.attributes.hp.max;
+        if (t.data.flags.lifeSteal === "undefined" || t.data.flags.lifeSteal === null) {
+            t.update({ "flags.lifeSteal": max})
+        };
+        let current = t.data.data.attributes.hp.value;
+        let newMax = max - args[2];
+        await t.update({ "data.attributes.hp.max": newMax });
+        if (t.data.data.attributes.hp.value > max) {
+            await t.update({ "data.attributes.hp.value": newMax });
+        };
+    } else {
+        if (t.data.flags.lifeSteal === "undefined" || t.data.flags.lifeSteal === null) {
+            return;
+        } else {
+            let oldMax = t.data.flags.lifeSteal;
+            await t.update({ "data.attributes.hp.max": oldMax });
+            await t.update({ "flags.lifeSteal": null });
+        };
+    };
+})();
