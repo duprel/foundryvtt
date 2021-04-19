4e3dbf6cc5014e309851cce11127f8ad4a1b1f2f
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-19 17:56:08 -0400

    incomplete

---
+++ 6th Level/harm.js
@@ -0,0 +1,15 @@
+﻿//args[1] = @target, args[2] = @damage
+(async () => {
+    if (args[0] === "on") {
+        let t = canvas.tokens.get(args[1]);
+        console.log("###################:       ", t.actor.data.data.attributes.hp.value)
+        if (t.actor.data.data.attributes.hp.value === 0) {
+            await t.actor.update({ "data.attributes.hp.value": 1 });
+            await game.cub.removeCondition("Dead", t.actor);
+        };
+        console.log("###################:       ", t.actor.data.data.attributes.hp.value)
+    } else {
+       
+       
+    };
+})();
