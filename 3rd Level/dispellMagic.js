77f6b34cd5dc233c9a7c5b4a39ba060754dbabcc
Lance duPre <ldupre237@gmail.com>
2021-04-18 21:07:14 -0400

    incomplete

---
+++ 3rd Level/dispellMagic.js
@@ -0,0 +1,6 @@
+﻿(async () => {
+    let t = args[0].actor;
+    let spellAbil = t.data.attributes.spellcasting;
+    let abilBonus = getProperty(t, `data.abilities.${spellAbil}.mod`);
+    console.log("##############:      ", abilBonus);
+})();
