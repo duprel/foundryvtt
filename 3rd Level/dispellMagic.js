8bd18997e64afd361252bd38f4ff8f587cc48a75
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-19 11:33:36 -0400

    complete

--- 3rd Level/dispellMagic.js
+++ 3rd Level/dispellMagic.js
@@ -2,5 +2,15 @@
     let t = args[0].actor;
     let spellAbil = t.data.attributes.spellcasting;
     let abilBonus = getProperty(t, `data.abilities.${spellAbil}.mod`);
-    console.log("##############:      ", abilBonus);
+    let dispelRoll = new Roll("d20 + @aBon", { aBon: abilBonus }).roll();
+    new MidiQOL.DamageOnlyWorkflow(actor, token, dispelRoll.total, "", "", dispelRoll, { flavor: "Dispel Magic Roll" });
+    if (dispelRoll._total < 13) {
+        var messageContent = "Dispel Magic failed!"
+    } else if (dispelRoll._total > 19) {
+        var messageContent = "Level 9 spells or less dispelled!"
+    } else {
+        var messageContent = "Level " + (dispelRoll._total - 10) + " spells or less dispelled!"
+    };
+    var chatData = { content: messageContent };
+    ChatMessage.create(chatData, {});
 })();
