6c9487e0a01396d7e28f6771703ff243e7680fb6
Lance duPre <ldupre237@gmail.com>
2021-04-19 21:32:44 -0400

    incomplete

--- 6th Level/harm.js
+++ 6th Level/harm.js
@@ -2,14 +2,10 @@
 (async () => {
     if (args[0] === "on") {
         let t = canvas.tokens.get(args[1]);
-        console.log("###################:       ", t.actor.data.data.attributes.hp.value)
         if (t.actor.data.data.attributes.hp.value === 0) {
             await t.actor.update({ "data.attributes.hp.value": 1 });
-            await game.cub.removeCondition("Dead", t.actor);
         };
         console.log("###################:       ", t.actor.data.data.attributes.hp.value)
     } else {
-       
-       
     };
 })();
