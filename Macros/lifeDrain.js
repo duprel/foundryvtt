b1c48bfd21f95e0dff81d59e25016a27ab4a5633
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-20 14:54:37 -0400

    complete

--- Macros/lifeDrain.js
+++ Macros/lifeDrain.js
@@ -1,24 +1,29 @@
-﻿//args[1] = @target, args[2] = @damage  
+﻿//args[1] = @target, args[2] = @damage
 (async () => {
-    let t = canvas.tokens.get(args[1]).actor;
+    let t = canvas.tokens.get(args[1]);
+    console.log("#########:     ", t);
     if (args[0] === "on") {
-        let max = t.data.data.attributes.hp.max;
-        if (t.data.flags.lifeSteal === "undefined" || t.data.flags.lifeSteal === null) {
-            t.update({ "flags.lifeSteal": max})
-        };
-        let current = t.data.data.attributes.hp.value;
+        let max = t.actor.data.data.attributes.hp.max;
+        let current = t.actor.data.data.attributes.hp.value;
         let newMax = max - args[2];
-        await t.update({ "data.attributes.hp.max": newMax });
-        if (t.data.data.attributes.hp.value > max) {
-            await t.update({ "data.attributes.hp.value": newMax });
+        if (t.actor.data.data.attributes.hp.value === 0) {
+            await t.actor.update({ "data.attributes.hp.value": 1 })
         };
+        await t.actor.update({ "data.attributes.hp.max": newMax });
+        if (current > newMax) {
+            await t.actor.update({ "data.attributes.hp.value": newMax });
+        };
+        if (t.actor.data.flags.world.lifeDrain === undefined || t.actor.data.flags.world.lifeDrain === null) {
+            t.actor.setFlag('world', 'lifeDrain', { value: max, });
+        };
+        console.log("#########:     ", current);
     } else {
-        if (t.data.flags.lifeSteal === "undefined" || t.data.flags.lifeSteal === null) {
+        if (t.actor.data.flags.world.lifeDrain === undefined || t.actor.data.flags.world.lifeDrain === null) {
             return;
-        } else {
-            let oldMax = t.data.flags.lifeSteal;
-            await t.update({ "data.attributes.hp.max": oldMax });
-            await t.update({ "flags.lifeSteal": null });
         };
+        let oldMax = t.actor.data.flags.world.lifeDrain.value;
+        console.log("#######:   ", oldMax)
+        await t.update({ "data.attributes.hp.max": oldMax });
+        t.actor.unsetFlag('world', 'lifeDrain');
     };
 })();
