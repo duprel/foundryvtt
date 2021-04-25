ef29bb4f21156dbdf70ad603fb8b4a7fe18c69d9
Lance duPre <ldupre237@gmail.com>
2021-04-25 17:26:54 -0400

    complete

---
+++ Macros/summonScript.js
@@ -0,0 +1,12 @@
+﻿//Macro args[1] = passed from Item Macro
+(async () => {
+    var a = canvas.tokens.controlled[0].actor;
+    if (args[0] === "on") {
+        await Summoner.placeAndSummon(
+            a,
+            args[1],
+        );
+        return;
+    };
+    Summoner.dismiss(args[1]);
+})();
