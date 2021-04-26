f85e9308bcbad6dcb461ecae7ea8dd3e0be11da3
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-04-26 14:41:56 -0400

    complete

---
+++ Items/glovesOfMissileSnaring.js
@@ -0,0 +1,11 @@
+﻿let a = canvas.tokens.controlled[0].actor;
+let damage = a.data.flags.dae.damgeApplied;
+let catchMis = new Roll("d10 + @abilities.dex.mod", a.getRollData()).roll();
+if (catchMis._total >= damage) {
+    catchMis.toMessage({ flavor: `${a.name} catches the missile! No damage!.`, });
+    a.update({ "data.attributes.hp.value": (a.data.data.attributes.hp.value + damage) });
+};
+if (catchMis._total < damage) {
+    catchMis.toMessage({ flavor: `${a.name} reduces damage by ${catchMis._total}.`, });
+    a.update({ "data.attributes.hp.value": (a.data.data.attributes.hp.value + damage - catchMis._total ) });
+};
---
+++ Items/potionOfVitality.js
@@ -0,0 +1,27 @@
+﻿(async () => {
+    if (args[0] === "on") {
+        if (game.cub.hasCondition("Diseased")) {
+            game.cub.removeCondition("Diseased");
+        };
+        if (game.cub.hasCondition("Exhaustion 1")) {
+            game.cub.removeCondition("Exhaustion 1");
+        };
+        if (game.cub.hasCondition("Exhaustion 2")) {
+            game.cub.removeCondition("Exhaustion 2");
+        };
+        if (game.cub.hasCondition("Exhaustion 3")) {
+            game.cub.removeCondition("Exhaustion 3");
+        };
+        if (game.cub.hasCondition("Exhaustion 4")) {
+            game.cub.removeCondition("Exhaustion 4");
+        };
+        if (game.cub.hasCondition("Exhaustion 5")) {
+            game.cub.removeCondition("Exhaustion 5");
+        };
+        if (game.cub.hasCondition("Poisoned")) {
+            game.cub.removeCondition("Poisoned");
+        };
+    } else {
+        return;
+    };
+})();
