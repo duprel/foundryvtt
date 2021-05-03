8dcad60768a74d40c7b03f57e544f09cb8c5a55f
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-03 11:42:18 -0400

    complete

---
+++ Macros/flumphAcidTendrils.js
@@ -0,0 +1,23 @@
+﻿//args[1] = @target = args[1]
+
+(async () => {
+    let tokenID = args[2].tokenId;
+    let t = canvas.tokens.get(tokenID);
+    if (args[0] === "on") {
+        let turnID = game.combat.combatants.find(i => i.tokenId == tokenID)._id;
+        const alertData = {
+            label: "Flumph Acid Damage",
+            round: 0,
+            roundAbsolute: false,
+            turnId: turnID,
+            endOfTurn: true,
+            repeating: {
+                frequency: 1,
+                expire: null,
+                expireAbsolute: false,
+            },
+            message: "Make a Constitution Save DC: 10 or take [[/roll 1d4]] acid damage!",
+        };
+        TurnAlert.create(alertData);
+    };
+})();
