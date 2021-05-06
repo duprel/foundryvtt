2dbfa4b3f45cec4c97a411b4b4b869171a761ab1
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-06 15:11:58 -0400

    complete

--- Macros/medusaPetrify.js
+++ Macros/medusaPetrify.js
@@ -1,15 +1,16 @@
 ﻿(async () => {
+    console.log("!!!!!!!!!!!:   ", args[0]);
     let t = canvas.tokens.get(args[1]).actor;
     if (args[0] === "on") {
         let saveRoll = new Roll("1d20 + @con", { con: t.data.data.abilities.con.saveBonus }).roll();
         if (saveRoll._total <= 9) {
             saveRoll.toMessage({ flavor: 'Saving Throw Failed! Petrified' });
-            game.cub.addCondition("Petrified", t);
+            await game.cub.addCondition("Petrified", t);
             return;
         };
         if (saveRoll._total > 9 && saveRoll._total < 14) {
             saveRoll.toMessage({ flavor: 'Saving Throw Failed! Restrained' });
-            game.cub.addCondition("Restrained", t);
+            await game.cub.addCondition("Restrained", t);
             return;
         };
         if (saveRoll._total >= 14) {
@@ -17,27 +18,30 @@
             return;
         };
     };
-
     if (args[0] === "each") {
-        if (game.cub.hasCondition("Restrained", t) {
+        if (game.cub.hasCondition("Restrained", t)) {
             let petRoll = new Roll("1d20 + @con", { con: t.data.data.abilities.con.saveBonus }).roll();
             if (petRoll._total < 14) {
                 petRoll.toMessage({ flavor: 'Saving Throw Failed! Petrified' });
-                game.cub.addCondition("Pertified", t);
+                await game.cub.removeCondition("Restrained", t);
+                await game.cub.addCondition("Petrified", t);
                 return;
             };
-            game.cub.removeCondition("Restrained", t);
-            game.cub.addCondition("Petrified", t);
-            return;
+            if (petRoll._total >= 14) {
+                petRoll.toMessage({ flavor: 'Saving Throw Passed!' });
+                await game.cub.removeCondition("Restrained", t);
+                return;
+            };
+
         };
     };
     if (args[0] === "off") {
         if (game.cub.hasCondition("Petrified", t)) {
-            game.cub.removeCondition("Petrified", t);
+            await game.cub.removeCondition("Petrified", t);
             return;
         };
         if (game.cub.hasCondition("Restrained")) {
-            game.cub.removeCondition("Restrained", t);
+            await game.cub.removeCondition("Restrained", t);
             return;
         };
     };
