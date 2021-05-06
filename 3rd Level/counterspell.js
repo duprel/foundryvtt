9bc1d75046c2446f36322d677298a22792c21eec
Lance duPre <ldupre237@gmail.com>
2021-05-05 20:36:25 -0400

    complete

---
+++ 3rd Level/counterspell.js
@@ -0,0 +1,16 @@
+﻿(async () => {
+    let t = args[0].actor;
+    let spellAbil = t.data.attributes.spellcasting;
+    let abilBonus = getProperty(t, `data.abilities.${spellAbil}.mod`);
+    let dispelRoll = new Roll("d20 + @aBon", { aBon: abilBonus }).roll();
+    new MidiQOL.DamageOnlyWorkflow(actor, token, dispelRoll.total, "", "", dispelRoll, { flavor: "Counterspell Roll" });
+    if (dispelRoll._total < 13) {
+        var messageContent = "Counterspell failed!"
+    } else if (dispelRoll._total > 19) {
+        var messageContent = "Level 9 spell or less countered!"
+    } else {
+        var messageContent = "Level " + (dispelRoll._total - 10) + " spell or less countered!"
+    };
+    var chatData = { content: messageContent };
+    ChatMessage.create(chatData, {});
+})();
---
+++ Macros/magminTouch.js
@@ -0,0 +1,5 @@
+﻿let t = canvas.tokens.get(args[1]);
+if (args[0] === "each") {
+    let damageRoll = new Roll("1d6[fire]").roll();
+    new MidiQOL.DamageOnlyWorkflow(actor, token, damageRoll.total, "fire", [t], damageRoll, { flavor: "Magmin Touch - Damage Roll (Fire)" });
+};
