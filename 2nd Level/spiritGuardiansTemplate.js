71ad362acae60f55a12bd6e557251b19443687c0
Lance duPre <ldupre237@gmail.com>
2021-05-07 16:45:52 -0400

    complete

--- 2nd Level/spiritGuardiansTemplate.js
+++ 2nd Level/spiritGuardiansTemplate.js
@@ -1,17 +1,20 @@
 ﻿(async () => {
     let t = canvas.tokens.get(args[1].tokenId);
+    const offset = 50;
+    const textr = "modules/jb2a_patreon/Library/3rd_Level/Spirit_Guardians/SpiritGuardians_01_Light_BlueYellow_600x600.webm";
+    const size = 15;
     if (args[0] === "on") {
-        let xPos = (t._validPosition.x + 50);
-        let yPos = (t._validPosition.y + 50);
+        let xPos = (t._validPosition.x + offset);
+        let yPos = (t._validPosition.y + offset);
         await MeasuredTemplate.create({
             t: "circle",
             user: game.user._id,
             x: xPos,
             y: yPos,
-            distance: 15,
+            distance: size,
             borderColor: "#FF0000",
             fillColor: "#FF3366",
-            texture: "modules/jb2a_patreon/Library/3rd_Level/Spirit_Guardians/SpiritGuardians_01_Light_BlueYellow_600x600.webm",
+            texture: textr,
             flags: {
                 DAESRD: {
                     SpiritGuardians: {
@@ -22,6 +25,10 @@
         });
         let template = canvas.templates.placeables.find(i => i.data.flags.DAESRD?.SpiritGuardians?.ActorId === t.actor.id);
         console.log("##############:    ", template, xPos);
-        await tokenAttacher.attachElementToToken(template, t, false)
+        await tokenAttacher.attachElementToToken(template, t, false);
+    };
+    if (args[0] === "off") {
+        let template = canvas.templates.placeables.find(i => i.data.flags.DAESRD?.SpiritGuardians?.ActorId === t.actor.id);
+        await canvas.templates.get(template.data._id).delete();
     };
 })();
