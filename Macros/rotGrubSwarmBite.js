20830707401faeb7aca005d4b7960b7ce37e57a5
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-10 10:01:37 -0400

    complete

--- Macros/rotGrubSwarmBite.js
+++ Macros/rotGrubSwarmBite.js
@@ -1,42 +1,23 @@
 ﻿// args[1] = token id
-let t = canvas.tokens.get(args[1]);
+
+const t = canvas.tokens.get(args[1]);
 if (args[0] === "on") {
     let newInfections = new Roll("d6").roll();
     if (t.actor.data.flags.world.rotGrubs === undefined || t.actor.data.flags.world.rotGrubs === null)  {
         await t.actor.setFlag("world", "rotGrubs", { value: newInfections._total });
         newInfections.toMessage({ flavor: `${t.data.name} total Rot Grub Infections: ${newInfections._total}` });
+        return;
+    };
+    if (t.actor.data.flags.world.rotGrubs != undefined && t.actor.data.flags.world.rotGrubs != null) {
+        let currInfections = t.actor.data.flags.world.rotGrubs.value + newInfections._total;
+        await t.actor.setFlag("world", "rotGrubs", { value: currInfections });
+        newInfections.toMessage({ flavor: `${t.data.name} total Rot Grub Infections: ${currInfections}` });
+        console.log("FARTON: ", currInfections);
+        return;
     };
 };
-
-
-
-
-
-
-
-
-//Roll for number of rot grub infections
-
-|| infections === 0
-//check rotgrub flag
-
- /** check rotgrub flag
-  * if null, undefined or 0
-    * set to 0
-        *     else
- * current number + new infections
- **/
-  
-
-/***
- * 
- * roll 1d6 = number of infections
- 
-
- *roll damage [nmber infections]d6
-        
-***/
-
-let t = canvas.tokens.get(args[1]);
-t.actor.unsetFlag('world', 'rotGrubs',);
-console.log("FART: ", t.actor.data.flags.world);
+if (args[0] === "each") {
+    let damDice = t.actor.data.flags.world.rotGrubs.value;
+    let damRoll = new Roll(`${damDice}d6[piercing]`).roll();
+    new MidiQOL.DamageOnlyWorkflow(actor, token, damRoll.total, "piercing", [t], damRoll, { flavor: "Rot Grub Damage Roll (Piercing)" });
+};
