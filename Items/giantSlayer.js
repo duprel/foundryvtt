3761ef04b909a34f2105982bd2fb6ec1e0afa5b5
Lance duPre <ldupre237@gmail.com>
2021-05-11 11:43:19 -0400

    complete

---
+++ Items/giantSlayer.js
@@ -0,0 +1,28 @@
+﻿(async () => {
+    function damRoll(damDice, damageType, actorD, tokenD, target) {
+        let damageRoll = new Roll(`${damDice}d6`).roll();
+        new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Giant Slayer (${damageType}) extra damage` });
+    };
+
+    if (args[0].hitTargets.length > 0) {
+    let damDice = 0;
+    let damageType = args[0].damageDetail[0].type;
+    let actorD = game.actors.get(args[0].actor._id);
+    let tokenD = canvas.tokens.get(args[0].tokenId);
+    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+    let giant = ["giant"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));   
+        if (giant) {
+            if (args[0].isCritical) {
+                damDice = 4;
+            };
+            if (!args[0].isCritical) {
+                damDice = 2;
+            };
+            await damRoll(damDice, damageType, actorD, tokenD, target);
+            let save_roll = await target.actor.rollAbilitySave('str', { chatMessage: true, fastForward: true });
+            if (save_roll._total < 15) {
+                game.cub.addCondition("Prone", target);
+            };
+        };
+    };
+})();
