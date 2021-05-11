6970e07c004cbaab2cce4e05910b230228944ba9
Lance duPre <ldupre237@gmail.com>
2021-05-11 08:24:48 -0400

    complete

--- Items/dragonSlayer.js
+++ Items/dragonSlayer.js
@@ -1,22 +1,23 @@
-﻿let actorD = game.actors.get(args[0].actor._id);
-let tokenD = canvas.tokens.get(args[0].tokenId);
-console.log("#############:    ", tokenD);
-
-let target = canvas.tokens.get(args[0].hitTargets[0]._id);
-let dragon = ["dragon"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
-if ((args[0].hitTargets.length > 0) && (args[0].isCritical)) {
+﻿if ((args[0].hitTargets.length > 0) && (args[0].isCritical)) {
+    let actorD = game.actors.get(args[0].actor._id);
+    let tokenD = canvas.tokens.get(args[0].tokenId);
+    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+    let dragon = ["dragon"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
     if (dragon) {
         let damageRoll = new Roll("6d6").roll();
         let damageType = args[0].damageDetail[0].type;
-        new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Dragon Slayer (${damageType})`, damageList: args[0].damageList, itemCardId: args[0].itemCardId });
-    }
+        new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Dragon Slayer (${damageType}) extra damage` });
+    };
     return;
-}
-if ((args[0].hitTargets.length > 0) {
+};
+if (args[0].hitTargets.length > 0) {
+    let actorD = game.actors.get(args[0].actor._id);
+    let tokenD = canvas.tokens.get(args[0].tokenId);
+    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+    let dragon = ["dragon"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
     if (dragon) {
         let damageRoll = new Roll("3d6").roll();
         let damageType = args[0].damageDetail[0].type;
-        new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Dragon Slayer (${damageType})`, damageList: args[0].damageList, itemCardId: args[0].itemCardId });
-    }
-}
-
+        new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Dragon Slayer (${damageType}) extra damage`});
+    };
+};
---
+++ Items/flameTongue.js
@@ -0,0 +1,17 @@
+﻿if ((args[0].hitTargets.length > 0) && (args[0].isCritical)) {
+    let actorD = game.actors.get(args[0].actor._id);
+    let tokenD = canvas.tokens.get(args[0].tokenId);
+    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+    let damageRoll = new Roll("4d6").roll();
+    let damageType = "fire";
+    new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Flame Tongue (${damageType}) extra damage` });
+    return;
+};
+if (args[0].hitTargets.length > 0) {
+    let actorD = game.actors.get(args[0].actor._id);
+    let tokenD = canvas.tokens.get(args[0].tokenId);
+    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+    let damageRoll = new Roll("2d6").roll();
+    let damageType = "fire";
+    new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Flame Tongue (${damageType}) extra damage`});
+};
