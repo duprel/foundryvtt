086b24abd7d00cef1a57d9eaae958ce3c6e409a8
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-19 13:06:25 -0400

    complete

---
+++ Items/sunBladeLighting.js
@@ -0,0 +1,20 @@
+﻿(async () => {
+    let t = canvas.tokens.controlled[0];
+    console.log("##########: ", t)
+    if (args[0] === "on") {
+        t.update({
+            dimLight: 30,
+            brightLight: 15,
+            lightAnimation: { type: "pulse", speed: 2, intensity: 2 },
+            lightAlpha: 0.15,
+            lightColor: "#f8c377"
+        }); 
+    };
+    if (args[0] === "off") {
+        t.update({
+            dimLight: 0,
+            brightLight: 0,
+            lightAnimation: { type: "pulse", speed: 2, intensity: 2 }
+        });
+    };
+})();
---
+++ Items/swordOfLifeStealing.js
@@ -0,0 +1,29 @@
+﻿(async () => {
+    if (args[0].hitTargets.length > 0) {
+        let damageType = "necrotic";
+        let actorD = game.actors.get(args[0].actor._id);
+        let tokenD = canvas.tokens.get(args[0].tokenId);
+        let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+        let wpnDmg = args[0].damageTotal;
+        let undead = ["undead"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
+        let construct = ["construct"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
+        if (args[0].isCritical) {
+            if (!undead && !construct) {
+                let damageRoll = new Roll(`3d6 + ${wpnDmg}`).roll();
+                new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Life Stealing (${damageType}) extra damage` });
+                let tHp = damageRoll.total - wpnDmg;
+                if (actorD.data.data.attributes.hp.temp != 0 || actorD.data.data.attributes.hp.temp != null || actorD.data.data.attributes.hp.temp != undefined) {
+                    if (actorD.data.data.attributes.hp.temp >= tHp) {
+                        return;
+                    };
+                    if (actorD.data.data.attributes.hp.temp < tHp) {
+                        actorD.update({ "data.attributes.hp.temp": wpnDmg });
+                    };
+                };
+                if (actorD.data.data.attributes.hp.temp === 0 || actorD.data.data.attributes.hp.temp === null || actorD.data.data.attributes.hp.temp === undefined) {
+                    actorD.update({ "data.attributes.hp.temp": wpnDmg });
+                };
+            };
+        };
+    };
+})();
