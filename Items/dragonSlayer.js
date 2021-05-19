663ccc142eb9dbbc31b3673a9a48b662fc81b368
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-19 10:32:39 -0400

    complete

--- Items/dragonSlayer.js
+++ Items/dragonSlayer.js
@@ -1,23 +1,24 @@
-﻿if ((args[0].hitTargets.length > 0) && (args[0].isCritical)) {
-    let actorD = game.actors.get(args[0].actor._id);
-    let tokenD = canvas.tokens.get(args[0].tokenId);
-    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
-    let dragon = ["dragon"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
-    if (dragon) {
-        let damageRoll = new Roll(`6d6 + ${args[0].damageTotal}`).roll();
-        let damageType = args[0].damageDetail[0].type;
+﻿(async () => {
+    function damRoll(damDice, damageType, actorD, tokenD, target, wpnDmg) {
+        let damageRoll = new Roll(`${damDice}d6 + ${wpnDmg}`).roll();
         new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Dragon Slayer (${damageType}) extra damage` });
     };
-    return;
-};
-if (args[0].hitTargets.length > 0) {
-    let actorD = game.actors.get(args[0].actor._id);
-    let tokenD = canvas.tokens.get(args[0].tokenId);
-    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
-    let dragon = ["dragon"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
-    if (dragon) {
-        let damageRoll = new Roll(`3d6 + ${args[0].damageTotal}`).roll();
+
+    if (args[0].hitTargets.length > 0) {
+        let damDice = 0;
         let damageType = args[0].damageDetail[0].type;
-        new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Dragon Slayer (${damageType}) extra damage`});
+        let actorD = game.actors.get(args[0].actor._id);
+        let tokenD = canvas.tokens.get(args[0].tokenId);
+        let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+        let dragon = ["dragon"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
+        if (dragon) {
+            if (args[0].isCritical) {
+                damDice = 6;
+            };
+            if (!args[0].isCritical) {
+                damDice = 3;
+            };
+            await damRoll(damDice, damageType, actorD, tokenD, target, args[0].damageTotal);
+        };
     };
-};
+})();
