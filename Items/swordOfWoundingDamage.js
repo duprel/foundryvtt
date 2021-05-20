5d9cb58a99fa4e60eec26d238b156ec6f3349baf
Lance duPre <ldupre237@gmail.com>
2021-05-20 08:14:08 -0400

    complete

---
+++ Items/swordOfWoundingDamage.js
@@ -0,0 +1,18 @@
+﻿(async () => {
+    let t = canvas.tokens.get(args[2].tokenId).actor;
+    if (args[0] === "on") {
+        if (t.data.flags.world === undefined || t.data.flags.world === null) {
+            t.setFlag('world', 'swordOfWounding');
+        };
+        if (t.data.flags.world.swordOfWounding === undefined || t.data.flags.world.swordOfWounding === null) {
+            t.setFlag('world', 'swordOfWounding', { maxHP: t.data.data.attributes.hp.max, });
+        };
+        let currMax = t.data.data.attributes.hp.max - args[1];
+        await t.update({ "data.attributes.hp.max": currMax });
+    };
+    if (args[0] === "off") {
+        console.log("###########", t.data.flags.world.swordOfWounding.maxHP);
+        await t.update({ "data.attributes.hp.max": t.data.flags.world.swordOfWounding.maxHP });
+        await t.unsetFlag('world', 'swordOfWounding');
+    };
+})();
