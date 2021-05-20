6636cb97a15ae1ddd2d992941d35bc00238b058e
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-20 15:26:13 -0400

    incomplete

---
+++ Items/viciousWeapon.js
@@ -0,0 +1,8 @@
+﻿if (args[0].attackRoll.results[0] === 20) {
+    let actorD = game.actors.get(args[0].actor._id);
+    let tokenD = canvas.tokens.get(args[0].tokenId);
+    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+    let damageType = args[0].damageDetail[0].type;
+    let damageRoll = new Roll("2d6").roll();
+    new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damageType, [target], damageRoll, { flavor: `Vicious Weapon (${damageType}) extra damage` });
+};
