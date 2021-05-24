5513c69f7acd5faf6b610a94bc69b0ae089f384d
DUPRE <duprel@COULIB-FSDUPRE2.local>
2021-05-24 13:44:45 -0400

    incomplete

---
+++ Items/ballLightning.js
@@ -0,0 +1,68 @@
+﻿function reName(end, aName, tName) {
+    for (let token of canvas.tokens.placeables) {
+        if (token.data.name.substring(0, end) === tName) {
+            token.actor.createOwnedItem({
+                "data": {
+                    "ability": "dex",
+                    "actionType": "save",
+                    "activation": {
+                        "cost": 1,
+                        "type": "bonus"
+                    },
+                    "damage": {
+                        "parts": [
+                            [
+                                "4d12",
+                                "lightning"
+                            ]
+                        ],
+                    },
+                    "range": {
+                        "units": "ft",
+                        "value": 5
+                    },
+                    "save": {
+                        "ability": "dex",
+                        "dc": 15,
+                        "scaling": "flat"
+                    },
+                    "target": {
+                        "type": "creature",
+                        "value": 1
+                    },
+                    "uses": {
+                        "max": 1,
+                        "per": "charges",
+                        "value": 1
+                    },
+                },
+                "img": "systems/dnd5e/icons/spells/light-sky-3.jpg",
+                "name": "Ball Lightning",
+                "type": "feat"
+            });
+            token.update({ name: aName + "'s " + tName });
+            token.update({ disposition: '1' });
+        };
+    };
+}
+
+let a = game.actors.get(args[1].actorId);
+let tName = "Ball Lightning";
+let end = 14;
+if (args[0] === "on") {
+    new Dialog({
+        title: "Choose the number of Ball Lightning Summoned.",
+        buttons: {
+            one: {
+                label: "1",
+                callback: () => {
+                    Summoner.placeAndSummon(
+                        a,
+                        tName,
+                    ).then(() => reName(end, a.data.name, tName));
+                }
+            }
+        }
+    }).render(true);
+
+};
