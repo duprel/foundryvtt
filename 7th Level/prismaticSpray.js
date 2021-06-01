8e7afa0ee5a00b362b801fa8024c9b9916630a82
Dupre <duprel@COULIB-FSDUPRE2.local>
2021-06-01 17:55:43 -0400

    incomplete

--- 7th Level/prismaticSpray.js
+++ 7th Level/prismaticSpray.js
@@ -1,38 +1,55 @@
 ﻿//DAE item macro
 
 (async () => {
-    let target = canvas.tokens.get(args[1]);
+    let damageRoll = async function (damType, a, tok, tar) {
+        let damage = new Roll(`10d10[${damType}]`).roll();
+        new MidiQOL.DamageOnlyWorkflow(a, tok, damage.total, damType, [tar], damage, { flavor: `Prismatic Spray (${damage}) (${damType}) damage` });
+    }
+
+
     if (args[0] == "on") {
-        var sprayRoll = new Roll("1d8").roll();
-        sprayRoll.toMessage({ flavor: `${target.name}'s roll` })
-        switch (sprayRoll.total) {
-            case 1:
-                ChatMessage.create({ content: "Red Ray!" });
-                break;
-            case 2:
-                ChatMessage.create({ content: "Orange Ray!" });
-                break;
-            case 3:
-                ChatMessage.create({ content: "Yellow Ray!" });
-                break;
-            case 4:
-                ChatMessage.create({ content: "Green Ray!" });
-                break;
-            case 5:
-                ChatMessage.create({ content: "Blue Ray!" });
-                break;
-            case 6:
-                ChatMessage.create({ content: "Indigo Ray!" });
-                break;
-            case 7:
-                ChatMessage.create({ content: "Violet Ray!" });
-                break;
-            case 8:
-                ChatMessage.create({ content: "Multiple Rays! Ignore this result if rolled again." });
-                break;
+        let target = canvas.tokens.get(args[0]);
+        console.log("###:", args[1]);
+        const table = game.tables.entities.find(t => t.name === "Prismatic Spray");
+        let r = await table.draw();
+        var x;
+        if (r.results.length === 1) {
+            x = 0
         };
-    console.log("#####: ", target, sprayRoll.total);
+        if (r.results.length > 1) {
+            x = 1
+        };
+        var i;
+        for (i = 0; i <= x; i++) {
+            let casePoint = r.results[i].text.charAt(0);
+            switch (casePoint) {
+                case "R":           
+                    damageRoll("fire", a, token, target);
+                    ChatMessage.create({ content: "Red Ray!" });
+                    break;
+                case "O":
+                    damageRoll("acid", a, token, target);
+                    ChatMessage.create({ content: "Orange Ray!" });
+                    break;
+                case "Y":
+                    damageRoll("lightning", a, token, target);
+                    ChatMessage.create({ content: "Yellow Ray!" });
+                    break;
+                case "G":
+                    damageRoll("poison", a, token, target);
+                    ChatMessage.create({ content: "Green Ray!" });
+                    break;
+                case "B":
+                    damageRoll("cold", a, token, target);
+                    ChatMessage.create({ content: "Blue Ray!" });
+                    break;
+                case "I":
+                    ChatMessage.create({ content: "Indigo Ray!" });
+                    break;
+                case "V":
+                    ChatMessage.create({ content: "Violet Ray!" });
+                    break;
+            };
+        };
     };
 })();
-
-
