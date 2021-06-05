63ba5faad5471fe2c19db77e997a5306de8bb1f0
Lance duPre <ldupre237@gmail.com>
2021-06-05 18:16:43 -0400

    incomplete

--- Items/nineLivesStealer.js
+++ Items/nineLivesStealer.js
@@ -5,10 +5,11 @@
         let target = canvas.tokens.get(args[0].hitTargets[0]._id);
         let undead = ["undead"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
         let construct = ["construct"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
-        if (giant || construct) {
+        if (undead || construct) {
             return;
         };
-
-        console.log("###: ", target)
+        target.actor.update({ "data.attributes.hp.value": 0 });
+        ChatMessage.create({ content: target.actor.data.name + "'s soul is absorbed!"});
+        console.log("###FART: ", target)
     };
 })();
