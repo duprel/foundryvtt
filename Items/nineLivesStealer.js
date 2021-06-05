80ebdcf4eeb3879ed857a5789df24e3b6d169d9a
Lance duPre <ldupre237@gmail.com>
2021-06-05 15:43:44 -0400

    incomplete

---
+++ Items/nineLivesStealer.js
@@ -0,0 +1,14 @@
+﻿//ItemMacro
+
+(async () => {
+    if (args[0].isCritical) {
+        let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+        let undead = ["undead"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
+        let construct = ["construct"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
+        if (giant || construct) {
+            return;
+        };
+
+        console.log("###: ", target)
+    };
+})();
