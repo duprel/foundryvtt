03b8456b75d4a1afeca68ab883a4d654541913ff
Dupre <duprel@COULIB-FSDUPRE2.local>
2021-06-08 16:54:19 -0400

    complete

--- 7th Level/prismaticSpray.js
+++ 7th Level/prismaticSpray.js
@@ -2,14 +2,24 @@
 
 (async () => {
     let damageRoll = async function (damType, a, tok, tar) {
+        let totDam = 0;
         let damage = new Roll(`10d10[${damType}]`).roll();
-        new MidiQOL.DamageOnlyWorkflow(a, tok, damage.total, damType, [tar], damage, { flavor: `Prismatic Spray (${damage}) (${damType}) damage` });
+        let save = await tar.actor.rollAbilitySave('dex', { chatMessage: true, fastForward: true });
+        if (save._total >= a.data.data.attr.spelldc) {
+            totDam = Math.floor(damage.total/2)
+        };
+        if (save._total < a.data.data.attr.spelldc) {
+            totDam = damage.total
+        };
+        console.log("###:", save._total, totDam);
+        new MidiQOL.DamageOnlyWorkflow(a, tok, totDam, damType, [tar], damage, { flavor: `Prismatic Spray (${damage.total}) (${damType}) damage` });
     }
 
 
     if (args[0] == "on") {
-        let target = canvas.tokens.get(args[0]);
-        console.log("###:", args[1]);
+        let actorD = game.actors.get(token.data.actorId);
+        let tokenD = canvas.tokens.get(token.data._id);
+        let target = canvas.tokens.get(args[1].tokenId);
         const table = game.tables.entities.find(t => t.name === "Prismatic Spray");
         let r = await table.draw();
         var x;
@@ -24,23 +34,23 @@
             let casePoint = r.results[i].text.charAt(0);
             switch (casePoint) {
                 case "R":           
-                    damageRoll("fire", a, token, target);
+                    damageRoll("fire", actorD, tokenD, target);
                     ChatMessage.create({ content: "Red Ray!" });
                     break;
                 case "O":
-                    damageRoll("acid", a, token, target);
+                    damageRoll("acid", actorD, tokenD, target);
                     ChatMessage.create({ content: "Orange Ray!" });
                     break;
                 case "Y":
-                    damageRoll("lightning", a, token, target);
+                    damageRoll("lightning", actorD, tokenD, target);
                     ChatMessage.create({ content: "Yellow Ray!" });
                     break;
                 case "G":
-                    damageRoll("poison", a, token, target);
+                    damageRoll("poison", actorD, tokenD, target);
                     ChatMessage.create({ content: "Green Ray!" });
                     break;
                 case "B":
-                    damageRoll("cold", a, token, target);
+                    damageRoll("cold", actorD, tokenD, target);
                     ChatMessage.create({ content: "Blue Ray!" });
                     break;
                 case "I":
---
+++ Items/hammerOfThunderbolts.js
@@ -0,0 +1,15 @@
+﻿//ImageMacro
+
+(async () => {
+    if (args[0].attackRoll.results[0] === 20) {
+        let target = canvas.tokens.get(args[0].hitTargets[0]._id);
+        let giant = ["giant"].some(type => (target.actor.data.data.details.type || "").toLowerCase().includes(type));
+        if (giant) {
+            let save_roll = await target.actor.rollAbilitySave('con', { chatMessage: true, fastForward: true });
+            if (save_roll._total < 17) {
+                target.actor.update({ "data.attributes.hp.value": 0 });
+                ChatMessage.create({ content: target.name + " is slain!" });
+            };
+        };
+    };
+})();
