08753af8795e632628fe18b1db2d89317d5851c4
Dupre <duprel@COULIB-FSDUPRE2.local>
2021-06-09 17:34:12 -0400

    incomplete

--- Items/rolmLifeDrain.js
+++ Items/rolmLifeDrain.js
@@ -1,25 +1,10 @@
 ﻿//ItemMacro
 
 (async () => {
-    if (args[0].hitTargets.length === 0) {
-        console.log("Missed or no target");
+    if (args[0].failedSaves.length === 0) {
+        console.log("Passed save or no target");
         return;
     };
-    let target = canvas.tokens.get(args[0].hitTargets[0]._id);
-    let save_roll = await target.actor.rollAbilitySave('con', { chatMessage: true, fastForward: true });
-    if (save_roll._total < 17) {
-        let a = game.actors.get(args[0].actor._id);
-        let tok = canvas.tokens.get(args[0].tokenId);
-        let damDice = 4;
-        if (args[0].isCritical) {
-            damDice = 8;
-        };
-        let damageRoll = new Roll(`${damDice}d6[necrotic]`).roll();
-        damageRoll.toMessage();
-        await new MidiQOL.DamageOnlyWorkflow(a, tok, damageRoll.total, "necrotic", [target], damageRoll, { flavor: `Rod of Lordly Might extra necrotic damage`, itemCardId: args[0].itemCardId, useOther: true, damageList: args[0].damageList });
-        let currHP = a.data.data.attributes.hp.value;
-        let maxHP = a.data.data.attributes.hp.max;
-        let hpHealed = Math.floor(damageRoll.total / 2);
-
-    };    
+    let a = game.actors.get(args[0].actor._id);
+    let hpHealed = Math.floor(args[0].damageTotal / 2);
 })();
