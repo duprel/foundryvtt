fcb8cb9c9febb56a57425c672a2d1efafbb93249
Dupre <duprel@COULIB-FSDUPRE2.local>
2021-06-09 10:50:02 -0400

    complete

---
+++ 5th Level/conjureAirElemental.js
@@ -0,0 +1,15 @@
+﻿//ItemMacro
+
+let tName = "Air Elemental";
+let end = 13;
+let aName = actor.data.name;
+await Summoner.placeAndSummon(
+    actor,
+    tName,
+);
+for (let token of canvas.tokens.placeables) {
+    if (token.data.name.substring(0, end) === tName) {
+        await token.update({ name: aName + "'s " + tName })
+        await token.update({ disposition: '1' })
+    }
+};
