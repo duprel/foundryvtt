9c745dc637dcae1d5148b06a7581b02b9c881620
Lance duPre <ldupre237@gmail.com>
2021-06-13 20:10:39 -0400

    complete

---
+++ Macros/succubusLifeDrain.js
@@ -0,0 +1,20 @@
+﻿(async () => {
+    const lastArg = args[args.length - 1];
+    let tarTok = canvas.tokens.get(lastArg.tokenId);
+    let tarAct = tarTok.actor;
+    if (args[0] === "on") {
+        let maxHP = tarAct.data.data.attributes.hp.max;
+        if (tarAct.data.flags.world === undefined || tarAct.data.flags.world === null) {
+            tarAct.setFlag('world', 'sucLifeDrain');
+        };
+        if (tarAct.data.flags.world.sucLifeDrain === undefined || tarAct.data.flags.world.sucLifeDrain === null) {
+            tarAct.setFlag('world', 'sucLifeDrain', { maxHP: maxHP });
+        };
+        let currMax = tarAct.data.data.attributes.hp.max - args[1];
+        await tarAct.update({ "data.attributes.hp.max": currMax });
+    };
+    if (args[0] === "off") {
+        await tarAct.update({ "data.attributes.hp.max": tarAct.data.flags.world.sucLifeDrain.maxHP });
+        await tarAct.unsetFlag('world', 'sucLifeDrain');
+    };
+})();
