90c20ec5a068b8c8bd5894bb2d169efc5d09bf2a
Lance duPre <ldupre237@gmail.com>
2021-06-24 14:50:27 -0400

    complete

--- 2nd Level/shadowBlade.js
+++ 2nd Level/shadowBlade.js
@@ -1,86 +1,89 @@
-﻿var a = canvas.tokens.controlled[0];
-if (args[0] === "on") {
-    if (canvas.tokens.controlled.length != 1) {
-        ui.notifications.error("Please select a single token.");
-        return;
-    };
-    let data = {};
+﻿//Itemmacro DAE @item.level
+
+(async () => {
     if (args[0] === "on") {
-        if (args[1] == 2) {
-            var weaponDamage = 2;
-        } else if (args[1] > 6) {
-            var weaponDamage = 5;
-        } else {
-            if (args[1] % 2 == 0) {
-                var weaponDamage = 1 + Math.floor(args[1] / 2);
-            } else {
-                var weaponDamage = 2 + Math.floor(args[1] / 2);
-            }
+        var data = {};
+        var weaponDamage;
+
+        if (args[1] === 2) {
+            weaponDamage = 2;
         };
+        if (args[1] === 3 || args[1] === 4) {
+            weaponDamage = 3;
+        };
+        if (args[1] === 5 || args[1] === 6) {
+            weaponDamage = 4;
+        };
+        if (args[1] > 6) {
+            weaponDamage = 5;
+        };
+
+        await actor.createOwnedItem(
+            {
+                "name": "Summoned Shadow Blade",
+                "type": "weapon",
+                "data": {
+                    "quantity": 1,
+                    "activation": {
+                        "type": "action",
+                        "cost": 1,
+                        "condition": ""
+                    },
+                    "duration": {
+                        "value": null,
+                        "units": ""
+                    },
+                    "target": {
+                        "value": 1,
+                        "width": null,
+                        "units": "",
+                        "type": "creature"
+                    },
+                    "range": {
+                        "value": 5,
+                        "long": null,
+                        "units": "feet"
+                    },
+                    "uses": {
+                        "value": 0,
+                        "max": 0,
+                        "per": ""
+                    },
+                    "consume": {
+                        "type": "",
+                        "target": "",
+                        "amount": null
+                    },
+                    "ability": "",
+                    "actionType": "mwak",
+                    "attackBonus": "0",
+                    "chatFlavor": "",
+                    "critical": null,
+                    "damage": {
+                        "parts": [
+                            [
+                                `${weaponDamage}d8`,
+                                "psychic"
+                            ]
+                        ],
+                        "versatile": ""
+                    },
+                    "weaponType": "simpleM",
+                    "proficient": true,
+                    "equipped": true,
+                    "properties": {
+                        "fin": true,
+                        "lgt": true,
+                        "thr": true
+                    },
+                },
+                "img": "modules/plutonium/media/icon/spell/xge-shadow-blade.jpg",
+            }
+        );
     };
-    await a.actor.createOwnedItem(
-        {
-            "name": "Summoned Shadow Blade",
-            "type": "weapon",
-            "data": {
-                "quantity": 1,
-                "activation": {
-                    "type": "action",
-                    "cost": 1,
-                    "condition": ""
-                },
-                "duration": {
-                    "value": null,
-                    "units": ""
-                },
-                "target": {
-                    "value": 1,
-                    "width": null,
-                    "units": "",
-                    "type": "creature"
-                },
-                "range": {
-                    "value": 5,
-                    "long": null,
-                    "units": "feet"
-                },
-                "uses": {
-                    "value": 0,
-                    "max": 0,
-                    "per": ""
-                },
-                "consume": {
-                    "type": "",
-                    "target": "",
-                    "amount": null
-                },
-                "ability": "",
-                "actionType": "mwak",
-                "attackBonus": "0",
-                "chatFlavor": "",
-                "critical": null,
-                "damage": {
-                    "parts": [
-                        [
-                            `${weaponDamage}d8`,
-                            "psychic"
-                        ]
-                    ],
-                    "versatile": ""
-                },
-                "weaponType": "simpleM",
-                "proficient": true,
-                "equipped": true,
-                "properties": {
-                    "fin": true,
-                    "lgt": true,
-                    "thr": true
-                },
-            },
-            "img": "modules/plutonium/media/icon/spell/xge-shadow-blade.jpg",
-        }
-    );
-} else {
-    let item = a.actor.data.items.find(i => i.name === "Summoned Shadow Blade" && i.type === "weapon")
-    a.actor.deleteOwnedItem(item._id)
-};
+
+    if (args[0] === "off") {
+        let item = actor.data.items.find(i => i.name === "Summoned Shadow Blade" && i.type === "weapon")
+        actor.deleteOwnedItem(item._id)
+    };
+})();
