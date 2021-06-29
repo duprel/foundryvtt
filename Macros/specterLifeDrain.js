75624c1049f11f758bbb29517e848da1c45f2a21
Lance duPre <ldupre237@gmail.com>
2021-06-29 19:30:10 -0400

    incomplete

---
+++ Macros/specterLifeDrain.js
@@ -0,0 +1,20 @@
+﻿(async () => {
+    const lastArg = args[args.length - 1];
+    let tarTok = canvas.tokens.get(lastArg.tokenId);
+    let tarAct = tarTok.actor;
+    if (args[0] === "on") {
+        let maxHP = tarAct.data.data.attributes.hp.max;
+        if (tarAct.data.flags.world === undefined || tarAct.data.flags.world === null) {
+            tarAct.setFlag('world', 'specterLifeDrain');
+        };
+        if (tarAct.data.flags.world.specterLifeDrain === undefined || tarAct.data.flags.world.specterLifeDrain === null) {
+            tarAct.setFlag('world', 'specterLifeDrain', { maxHP: maxHP });
+        };
+        let currMax = tarAct.data.data.attributes.hp.max - args[1];
+        await tarAct.update({ "data.attributes.hp.max": currMax });
+    };
+    if (args[0] === "off") {
+        await tarAct.update({ "data.attributes.hp.max": tarAct.data.flags.world.specterLifeDrain.maxHP });
+        await tarAct.unsetFlag('world', 'specterLifeDrain');
+    };
+})();
